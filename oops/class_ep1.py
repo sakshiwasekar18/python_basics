bb6c205a7ad410abb381643277baa8d14e3642f8
sakshiwasekar18 <wasekarsakshi@gmail.com>
2026-05-21 15:25:24 +0530

    Added Python class file

---
+++ oops/class_ep1.py
@@ -0,0 +1,21 @@
+class mobile:
+  #method 
+  def show_details(self):
+    print("brand:",self.brand)
+    print("model_no:",self.model_no)
+
+  #creating an object
+m1=mobile()
+  
+  #assignment values 
+m1.brand="realme"
+m1.model_no="123dcf"
+
+  #calling method 
+m1.show_details()
+
+
+
+
+
+
